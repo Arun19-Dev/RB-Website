67616b4faa818dcec0228bed0e3c6ca3a1241d6b
arun-19n <nuraruran32@gmail.com>
2025-12-10 19:58:27 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: "/RB-Website/",
+
   define: {
     'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL || 'http://localhost:8000')
   },
